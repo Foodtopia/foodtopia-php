b576660920f9013d452ca796ba54a13d1690fc8b
shinder <lsd0125@gmail.com>
2018-11-05 10:09:35 +0800

    list page structure

--- ab_list.php
+++ ab_list.php
@@ -37,8 +37,37 @@
     }
 </style>
 
-<div class="container">
-    <h2>Hello</h2>
+<div class="container" style="margin-top: 20px">
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        </tbody>
+    </table>
 </div>
 
 
