e0cb3b6e541a2b7b3256c3ef9cc4cf3b8ceea772
shinder <lsd0125@gmail.com>
2018-11-05 10:43:11 +0800

    page blocks

--- ab_index.php
+++ ab_index.php
@@ -1,47 +1,8 @@
-<!doctype html>
-<html lang="zh">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="bootstrap/js/bootstrap.js"></script>
-    <title>Document</title>
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-        <a class="navbar-brand" href="#">通訊錄</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-<style>
-    li.active{
-        background-color: #3581bf;
-    }
-</style>
+<?php include __DIR__. '/__html_head.php'; ?>
+<?php include __DIR__. '/__navbar.php'; ?>
 
 <div class="container">
     <h2>Hello</h2>
 </div>
 
-
-</body>
-</html>
-
+<?php include __DIR__. '/__html_foot.php';
--- ab_list.php
+++ ab_list.php
@@ -2,47 +2,9 @@
 require __DIR__. '/__connect_db.php';
 
 $stmt = $pdo->query("SELECT * FROM address_book");
-
 ?>
-<!doctype html>
-<html lang="zh">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="bootstrap/js/bootstrap.js"></script>
-    <title>Document</title>
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-        <a class="navbar-brand" href="#">通訊錄</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-<style>
-    li.active{
-        background-color: #3581bf;
-    }
-</style>
-
+<?php include __DIR__. '/__html_head.php'; ?>
+<?php include __DIR__. '/__navbar.php'; ?>
 <div class="container" style="margin-top: 20px">
     <table class="table table-striped table-bordered">
         <thead>
@@ -69,8 +31,4 @@ $stmt = $pdo->query("SELECT * FROM address_book");
         </tbody>
     </table>
 </div>
-
-
-</body>
-</html>
-
+<?php include __DIR__. '/__html_foot.php';
