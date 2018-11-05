5e550183d7b25d6972ddac6ccbe635425e25fdf1
shinder <lsd0125@gmail.com>
2018-11-05 09:34:55 +0800

    statement fetch

--- hello_pdo.php
+++ hello_pdo.php
@@ -1,10 +1,18 @@
 <?php
 
 require __DIR__. '/__connect_db.php';
+try {
+    //$stmt = $pdo->query("SELECT * FROM address_book");
+    $stmt = $pdo->query("SELECT `email`,`name` FROM address_book");
+    //$stmt = $pdo->query("SELECT 2+3"); //測試除錯用
 
-$stmt = $pdo->query("SELECT * FROM address_book");
+    //$data_ar = $stmt->fetchAll(); // 兩種都給
+    //$data_ar = $stmt->fetchAll(PDO::FETCH_ASSOC); // 只給關聯式的陣列
+    $data_ar = $stmt->fetch(PDO::FETCH_ASSOC); //一筆一筆拿
 
-$data_ar = $stmt->fetchAll();
+    echo json_encode($data_ar, JSON_UNESCAPED_UNICODE);
+}catch (PDOException $ex){
+    echo 'query failed:'. $ex->getMessage();
+}
 
-echo json_encode($data_ar, JSON_UNESCAPED_UNICODE);
 
